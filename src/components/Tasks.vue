f7a704f9bfbf6ddc08a0d0629c2c76018b4f27a8
QuangLong <quang.long292@gmail.com>
2021-05-01 21:46:57 +0700

    handling delete

---
+++ src/components/Tasks.vue
@@ -0,0 +1,19 @@
+<template>
+    <div :key="item.id" v-for="item in tasks">
+        <Task :task="item"/>
+    </div>
+</template>
+
+<script>
+import Task from "./Task";
+
+export default {
+    name: 'Tasks',
+    props: {
+        tasks: Array
+    },
+    components: {
+        Task
+    }
+}
+</script>
